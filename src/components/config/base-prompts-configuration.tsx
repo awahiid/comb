d35151e00b8d8badf7e8f9152e71e98475aa8923
abdel <awahiid@proton.me>
2026-02-27 18:51:42 +0100

    Added description base prompt configuration option

--- src/components/config/base-prompts-configuration.tsx
+++ src/components/config/base-prompts-configuration.tsx
@@ -7,11 +7,15 @@ export function BasePromptsConfiguration({config, onChange}: ConfigComponentProp
     return <>
         <Label className={"flex flex-col items-start"}>
             <span className={"font-bold"}>Subject base prompt</span>
-            <Textarea value={config.contentBasePrompt} onChange={e => onChange("contentBasePrompt", e.target.value)}/>
+            <Textarea value={config.subjectBasePrompt} onChange={e => onChange("subjectBasePrompt", e.target.value)}/>
         </Label>
         <Label className={"flex flex-col items-start"}>
             <span className={"font-bold"}>Email base prompt</span>
-            <Textarea value={config.subjectBasePrompt} onChange={e => onChange("subjectBasePrompt", e.target.value)}/>
+            <Textarea value={config.contentBasePrompt} onChange={e => onChange("contentBasePrompt", e.target.value)}/>
+        </Label>
+        <Label className={"flex flex-col items-start"}>
+            <span className={"font-bold"}>Description base prompt</span>
+            <Textarea value={config.descriptionBasePrompt} onChange={e => onChange("descriptionBasePrompt", e.target.value)}/>
         </Label>
     </>;
 }
