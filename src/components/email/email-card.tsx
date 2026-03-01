fc4b69d4f6ed2be8dc93c7d9c2a550fe1267ab4c
abdel <awahiid@proton.me>
2026-03-01 14:03:24 +0100

    Redesign of email card

--- src/components/email/email-card.tsx
+++ src/components/email/email-card.tsx
@@ -9,9 +9,11 @@ import {useShallow} from "zustand/shallow";
 import {formatDate} from "@/lib/utils";
 
 export default function EmailCard(){
-    const {company, status, sentOn, messageId} = useCompanyStore(
+    const {company, name, email, status, sentOn, messageId} = useCompanyStore(
         useShallow(state => ({
             company: state.company,
+            name: state.company?.name,
+            email: state.company?.email,
             status: state.company?.status,
             sentOn: state.company?.sentOn,
             messageId: state.company?.messageId
@@ -24,11 +26,11 @@ export default function EmailCard(){
         return <Card className={"size-full max-w-3xl h-fit rounded-xs"}>
                 <CardHeader>
                     <CardTitle className={"flex items-center w-full h-fit"}>
-                        Mail
+                        Email
                     </CardTitle>
                 </CardHeader>
                 <CardContent className={"flex flex-col gap-2"}>
-                    <p>The email to this company was sent successfully on {sentOn && formatDate(sentOn)}</p>
+                    <p>The email to <span className={"font-bold"}>{name}</span> ({email}) was successfully sent on {sentOn && formatDate(sentOn)}</p>
                     <p>Message Id {messageId}</p>
                 </CardContent>
             </Card>
@@ -37,7 +39,7 @@ export default function EmailCard(){
     return <Card className={"size-full max-w-3xl h-fit rounded-xs"}>
         <CardHeader>
             <CardTitle className={"flex items-center w-full h-fit"}>
-                Mail
+                Email
             </CardTitle>
             <EmailAddress/>
             <EmailSubject/>
