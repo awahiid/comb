61885bebdbb19e60940549e0f22ed8e7187373a7
abdel <awahiid@proton.me>
2026-03-02 11:55:07 +0100

    Added electron to create desktop distributions

---
+++ electron/main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from "electron"
+import { spawn, ChildProcess } from "child_process"
+
+let nextProcess: ChildProcess | null = null
+let mainWindow: BrowserWindow | null = null
+
+function createWindow(): void {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+    })
+
+    mainWindow.loadURL("http://localhost:3000")
+}
+
+app.whenReady().then(() => {
+    nextProcess = spawn("npx", ["next", "start", "-p", "3000"], {
+        shell: true,
+        stdio: "inherit",
+    })
+
+    setTimeout(createWindow, 3000)
+})
+
+app.on("will-quit", () => {
+    if (nextProcess) nextProcess.kill()
+})
