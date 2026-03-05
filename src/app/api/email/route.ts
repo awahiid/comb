8ec84788a016aff2749d5da4dc15b7e4fa690888
abdel <awahiid@proton.me>
2026-03-05 23:28:41 +0100

    Deleted unused code.

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,5 +1,4 @@
 import nodemailer from "nodemailer"
-import { ImapFlow } from "imapflow";
 
 export async function POST(req: Request) {
     const formData = await req.formData()
@@ -13,8 +12,6 @@ export async function POST(req: Request) {
     const content = formData.get("content") as string
 
     const files = formData.getAll("attachments") as File[]
-
-    console.log(address)
 
     const attachments = await Promise.all(
         files.map(async (file) => {
@@ -57,43 +54,4 @@ export async function POST(req: Request) {
         accepted: info.accepted,
         rejected: info.rejected
     }), { status: 200 })
-}
-
-export async function GET(req: Request) {
-    const formData = await req.formData();
-
-    const user = formData.get("user") as string;
-    const pass = formData.get("pass") as string;
-    const hostname = formData.get("hostname") as string;
-    const port = Number(formData.get("port") as string);
-    const messageId = formData.get("messageId") as string;
-
-    const client = new ImapFlow({
-        host: hostname,
-        port,
-        secure: true,
-        auth: { user, pass }
-    });
-
-    await client.connect();
-    await client.mailboxOpen("INBOX");
-
-    // for await (const msg of client.fetch(
-    //     { header: { messageId } },
-    //     { source: true, uid: true }
-    // )) {
-    //     const parsed = simpleParser(msg.source);
-    //     await client.logout();
-    //
-    //     return new Response(JSON.stringify({
-    //         uid: msg.uid,
-    //         subject: parsed.subject,
-    //         from: parsed.from?.text,
-    //         date: parsed.date,
-    //         text: parsed.text
-    //     }), { headers: { "Content-Type": "application/json" }});
-    // }
-    //
-    // await client.logout();
-    // return new Response("Message not found", { status: 404 });
 }
