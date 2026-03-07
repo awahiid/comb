e0f7aebe5437c01ad78708ce2fec7dd951d41424
abdel <awahiid@proton.me>
2026-03-07 16:44:56 +0100

    Body bg gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,14 @@ import SaveDataButton from "@/components/layout/save-data-button";
 export default function Home() {
 
   return <>
+      <div className="absolute inset-0 -z-50 bg-gradient-to-b from-transparent to-secondary-foreground"></div>
       <main className="flex justify-center gap-4 h-9/10 max-h-[900] pt-10 px-10 ">
           <CompanyCard/>
           <EmailCard/>
           <DataTable/>
       </main>
       <footer className={"px-10 w-xl mx-auto flex h-1/10 pt-4"}>
-          <nav className="border-black h-fit mx-auto flex py-2 px-4 rounded-full border w-full bg-card items-center justify-between">
+          <nav className="h-fit mx-auto flex py-2 px-4 rounded-full border w-full bg-card items-center justify-between">
               <div className={"flex items-center gap-1 justify-baseline"}>
                   <CombLogo className="w-5 h-auto rotate-180" />
                   <p className={"font-bold text-lg mb-1"}>comb</p>
--- src/components/email/email-content.tsx
+++ src/components/email/email-content.tsx
@@ -29,7 +29,7 @@ export default function EmailContent() {
         <CardContent className={"h-full"}>
             <Textarea
                 value={content}
-                className={"field-sizing-fixed border-black rounded-md bg-secondary p-4 shadow-none resize-none focus-visible:ring-[0px] h-full"}
+                className={"field-sizing-fixed rounded-md bg-secondary p-4 shadow-none resize-none focus-visible:ring-[0px] h-full"}
                 onChange={(e) => set("content", e.target.value)}
             />
         </CardContent>
