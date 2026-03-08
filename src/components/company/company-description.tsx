395de58780a3b615421f298b7905868e5ec72b54
abdel <awahiid@proton.me>
2026-03-08 03:32:18 +0100

    Generating message on loading description

--- src/components/company/company-description.tsx
+++ src/components/company/company-description.tsx
@@ -1,7 +1,7 @@
 import {useCompanyStore} from "@/stores/use-company-store";
 import {useShallow} from "zustand/shallow";
 import {useConfigurationStore} from "@/stores/use-configuration-store";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Textarea} from "@/components/ui/textarea";
 import {Skeleton} from "@/components/ui/skeleton";
 import {Button} from "@/components/ui/button";
@@ -10,8 +10,9 @@ import CombAI from "@/assets/comb-ai.svg";
 export default function CompanyDescription() {
     const basePrompt = useConfigurationStore(state => state.config.descriptionBasePrompt);
 
-    const { savedDescription, saveDescription, generateDescription } = useCompanyStore(
+    const { id, savedDescription, saveDescription, generateDescription } = useCompanyStore(
         useShallow(state => ({
+            id: state.id,
             savedDescription: state.description ?? "",
             saveDescription: state.set,
             generateDescription: state.generateDescription
@@ -34,6 +35,10 @@ export default function CompanyDescription() {
         }
     };
 
+    useEffect(() => {
+        if(!savedDescription) generate()
+    }, [id])  // eslint-disable-line react-hooks/exhaustive-deps
+
     return <>
         {!loading && <Textarea
             className="field-sizing-fixed h-full bg-secondary border-x-0 pb-2 min-h-lh border-t-0 border-gray-300 focus-visible:ring-0 focus:border-black focus:outline-none resize-none no-scrollbar"
@@ -41,7 +46,7 @@ export default function CompanyDescription() {
             placeholder={"No description yet."}
             onChange={e => setDescription(e.target.value)}
         />}
-        {loading && <Skeleton className="h-full"/>}
+        {loading && <Skeleton className="h-full p-4">Generating description...</Skeleton>}
         <div className={"flex items-center py-4"}>
             <p className={"text-sm text-nowrap"}> {description == savedDescription ? "Saved" : "Not saved"} </p>
             <div className={"flex h-fit w-full justify-end items-center gap-2"}>
